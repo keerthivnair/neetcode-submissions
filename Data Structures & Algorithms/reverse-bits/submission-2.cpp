cd7f7366ed40549116deebb5470fcd4cc8ccea85
Keerthi V Nair <144615089+keerthivnair@users.noreply.github.com>
2026-07-14 21:37:45 +0530

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        vector<int> a;
+
+        // Store all 32 bits
+        for (int i = 0; i < 32; i++) {
+            a.push_back(n & 1);
+            n >>= 1;
+        }
+
+        uint32_t ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if (a[i]) {
+                ans |= (1u << (31 - i));
+            }
+        }
+
+        return ans;
+    }
+};
