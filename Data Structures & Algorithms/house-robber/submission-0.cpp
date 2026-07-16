dc6cabfa57cf2a3c213d5674594f4d9743265b79
Keerthi V Nair <144615089+keerthivnair@users.noreply.github.com>
2026-07-16 20:05:34 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+      int n = nums.size();
+      vector<int>dp(n+1);
+      nums.insert(nums.begin(), 0);
+      dp[1] = nums[1];
+      for(int i=2;i<=n;i++) {
+        dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+      }
+      return dp[n];
+    }
+};
